06d5225109146fa28fdb5c1c478145409418e46c
Ben Anhalt <ben.anhalt@gmail.com>
2012-09-20 20:54:18 -0500

    done?

--- xinterp.rkt
+++ xinterp.rkt
@@ -34,9 +34,9 @@
       [(list-rest op operands)
        (cond
         [(member op '(+ - * /)) (build-binop op operands)]
-        [(symbol=? 'with op) (build-with operands)]
-        [(symbol=? 'if0 op) (build-if operands)]
-        [(symbol=? 'fun op) (build-fun operands)]
+        [(eq? 'with op) (build-with operands)]
+        [(eq? 'if0 op) (build-if operands)]
+        [(eq? 'fun op) (build-fun operands)]
         [else (app (parse op) (map parse operands))])]
       [_ (error "parse error" sexp)])]
     [else (error "parse error" sexp)]))
@@ -50,7 +50,8 @@
 
 (define (build-binop op operands)
   (match operands
-    [(list l r) (binop (eval op) (parse l) (parse r))]
+    [(list l r) (binop (second (assoc op `((+ ,+) (- ,-) (* ,*) (/ ,/))))
+                       (parse l) (parse r))]
     [_ (error "wrong number of operands for binop")]))
 
 (define (build-with operands)
@@ -100,8 +101,27 @@
     [with (binds body) (intrp body (extend-env binds env))]
     [id (name) (lookup name env)]
     [if0 (c t e) (intrp (do-if0 (intrp c env) t e) env)]
-    [fun (args body) (error "not implemented")]
-    [app (f args) (error "not implemented")]))
+    [fun (args body) (closureV args body env)]
+    [app (f args) (do-app (intrp f env)
+                          (map (lambda (a) (intrp a env)) args))]))
+
+;; do-app : CFWAE-Value * (listof CFWAE-Value) : CFWAE-Value
+(define (do-app clos args)
+  (type-case CFWAE-Value clos
+    [closureV (params body env)
+               (intrp body (add-args-to-env params args env))]
+    [else (error "can't apply non-function value" clos)]))
+
+;; add-args-to-env : (listof symbol) * (listof CFWAE-Value) * Env -> Env
+(define (add-args-to-env params args env)
+  (cond
+   [(and (empty? params) (empty? args)) env]
+   [(empty? params) (error "too many arguments in application")]
+   [(empty? args) (error "too few arguments in application")]
+   [else (add-args-to-env
+          (rest params)
+          (rest args)
+          (anEnv (first params) (first args) env))]))
 
 ;; lookup : symbol * Env -> CFWAE-Value
 (define (lookup name env)
@@ -111,9 +131,6 @@
            (if (symbol=? n name) val
                (lookup name more-env))]))
 
-(test/exn (lookup 'a (mtEnv)) "unbound identifier")
-(test (lookup 'a (anEnv 'a (numV 0) (mtEnv))) (numV 0))
-
 ;; do-binop : operation * CFWAE-Value * CFWAE-Value -> CFWAE-Value
 (define (do-binop op l r)
   (cond
@@ -122,25 +139,11 @@
    [(and (equal? op /) (equal? r (numV 0))) (error "div by zero")]
    [else (numV (op (numV-n l) (numV-n r)))]))
 
-(test (do-binop + (numV 3) (numV 4)) (numV 7))
-(test (do-binop - (numV 4) (numV 6)) (numV -2))
-(test (do-binop * (numV 2) (numV 5)) (numV 10))
-(test (do-binop / (numV 100) (numV 10)) (numV 10))
-
-(test/exn (do-binop > (numV 5) (numV 4)) "unsupported operation")
-(test/exn (do-binop + (numV 5) (closureV '(a b c) (num 4) (mtEnv)))
-          "one operand was not a number value")
-(test/exn (do-binop / (numV 1) (numV 0)) "div by zero")
-
 ;; do-if0 : CFWAE-Value * CFWAE * CFWAE -> CFWAE
 (define (do-if0 pred then alt)
   (type-case CFWAE-Value pred
     [numV (n) (if (equal? n 0) then alt)]
     [else (error "non-numeric predicate")]))
-
-(test (do-if0 (numV 0) (num 1) (num 2)) (num 1))
-(test (do-if0 (numV 1) (num 1) (num 2)) (num 2))
-(test/exn (do-if0 (closureV '(a) (num 0) (mtEnv)) (num 1) (num 2)) "non-numeric predicate")
 
 ;; extend-env : (listof Binding) * Env -> Env
 (define (extend-env binds orig-env)
@@ -178,10 +181,6 @@
             (test/exn (parse bad) "parse error"))
           '(fun if0 with + / - *))
 
-(for-each (lambda (op)
-            (test (parse (list op '4 '5))
-                  (binop (eval op) (num 4) (num 5))))
-          '(+ - / *))
 (test/exn (parse '{+}) "wrong number of operands for binop")
 (test/exn (parse '{+ 1}) "wrong number of operands for binop")
 (test/exn (parse '{+ 1 2 3}) "wrong number of operands for binop")
@@ -216,3 +215,69 @@
 (test (parse '{f a b c}) (app (id 'f) (list (id 'a) (id 'b) (id 'c))))
 (test/exn (parse '{}) "parse error")
 
+(test (do-binop + (numV 3) (numV 4)) (numV 7))
+(test (do-binop - (numV 4) (numV 6)) (numV -2))
+(test (do-binop * (numV 2) (numV 5)) (numV 10))
+(test (do-binop / (numV 100) (numV 10)) (numV 10))
+
+(test/exn (do-binop > (numV 5) (numV 4)) "unsupported operation")
+(test/exn (do-binop + (numV 5) (closureV '(a b c) (num 4) (mtEnv)))
+          "one operand was not a number value")
+(test/exn (do-binop / (numV 1) (numV 0)) "div by zero")
+
+(test (do-if0 (numV 0) (num 1) (num 2)) (num 1))
+(test (do-if0 (numV 1) (num 1) (num 2)) (num 2))
+(test/exn (do-if0 (closureV '(a) (num 0) (mtEnv)) (num 1) (num 2)) "non-numeric predicate")
+
+(test/exn (lookup 'a (mtEnv)) "unbound identifier")
+(test (lookup 'a (anEnv 'a (numV 0) (mtEnv))) (numV 0))
+
+
+(define (ev sexp)
+  (numV-n (interp (parse sexp))))
+
+(test (ev '{+ 4 5}) 9)
+(test (ev '{/ 10 5}) 2)
+(test (ev '{- 4 5}) -1)
+(test (ev '{* 4 5}) 20)
+
+(test (ev '{with {{x 3} {y 4}} {+ x y}}) 7)
+(test (ev '{with {{x 2}
+                  {y 3}}
+                 {with {{z {+ x y}}}
+                       {+ x z}}})
+      7)
+
+(test/exn (ev '{with {{x 2}
+                      {x 3}}
+                     {+ x 2}})
+          "duplicate name in binding list")
+
+(test (ev '{{fun {x y} {* x y}} 2 3}) 6)
+(test/exn (ev '{{fun {x x} {* x x}} 4 5}) "duplicate args")
+(test (ev '{{fun {} 5}}) 5)
+(test/exn (ev '{{fun {} 5} 4}) "too many arguments")
+(test/exn (ev '{{fun {x y} x} 5}) "too few arguments")
+
+(test/exn (ev '{with {{x 10} {x 20}} 50}) "duplicate name in binding list")
+
+(test/exn (ev '{{+ 3 4} 5}) "can't apply non-function value")
+
+(test (ev '{with {{x 10}} {{fun {y} x} 20}}) 10)
+(test (ev '{with {{x 10} {y 20}} {{fun {x} {+ x y}} 30}}) 50)
+(test (ev '{{fun {x y} {* {with {{y 10}} {+ x y}} y}} 2 3}) 36)
+(test (ev '{with {{f {fun {x} {* x x}}}} {f 2}}) 4)
+(test (ev '{with {{f {fun {x} {* x x}}}} {f {f 2}}}) 16)
+
+(test (ev
+       '{with {{Y {fun {f}
+                       {{fun {x} {f {fun {v} {{x x} v}}}}
+                        {fun {x} {f {fun {v} {{x x} v}}}}}}}}
+
+              {with {{fact
+                      {Y {fun {f}
+                              {fun {n}
+                                   {if0 n 1
+                                        {* n {f {- n 1}}}}}}}}}
+                    {fact 5}}})
+      120)
